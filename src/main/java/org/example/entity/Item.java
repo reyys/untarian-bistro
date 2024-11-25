0186b006d9758576e68ddb0c03a09c8a21a73d8d
Velosofy <61261357+Velosofy@users.noreply.github.com>
2024-11-25 18:59:06 +0700

    Move to SQL2O Library (#2)

--- src/main/java/org/example/entity/Item.java
+++ src/main/java/org/example/entity/Item.java
@@ -1,4 +1,6 @@
-package org.example;
+package org.example.entity;
+
+import org.example.Utility;
 
 public class Item {
     private int id;
---
+++ src/main/java/org/example/repository/BaseRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import java.util.List;
+
+public interface BaseRepository<T> {
+    T save(T entity);
+    List<T> findAll();
+    T findById(int id);
+    void update(T entity);
+    void deleteById(int id);
+    int getLastId();
+}
