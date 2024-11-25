4e2f6460bdb3d04d6e2bec1a3bea562085385a10
reyys <reyyy0207@gmail.com>
2024-11-25 18:36:48 +0700

    Convert JDBC to sql2o

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,117 +1,99 @@
 package org.example;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 class Menu {
-    private Connection conn;
+    private Sql2o sql2o;
 
     public Menu() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/UntarianBistro", "root", "");
+        this.sql2o = new Sql2o("jdbc:mysql://localhost:3306/UntarianBistro", "root", "");
     }
 
     public boolean isConnected() {
-        return (this.conn != null);
-    }
-
-    public void closeConnection() {
-        try {
-            this.conn.close();
-        } catch (SQLException e) {
+        try (Connection conn = sql2o.open()) {
+            return conn != null;
+        } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
     }
 
+
     // Create (Insert a new menu item)
-    public void insertMenuItem(Item item) throws SQLException {
-        String query = "INSERT INTO menu(name, price, description, image_url, created_at, updated_at, stock) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        PreparedStatement stmt = this.conn.prepareStatement(query);
-        stmt.setString(1, item.getName());
-        stmt.setDouble(2, item.getPrice());
-        stmt.setString(3, item.getDescription());
-        stmt.setString(4, item.getImageUrl());
-        stmt.setString(5, item.getCreatedAt());
-        stmt.setString(6, item.getUpdatedAt());
-        stmt.setInt(7, item.getStock());
-        stmt.executeUpdate();
+    public int insertMenuItem(Item item) {
+        String query = "INSERT INTO menu(name, price, description, image_url, created_at, updated_at, stock) " +
+                "VALUES (:name, :price, :description, :imageUrl, :createdAt, :updatedAt, :stock)";
+        try (Connection conn = sql2o.open()) {
+            int id = conn.createQuery(query, true)
+                    .addParameter("name", item.getName())
+                    .addParameter("price", item.getPrice())
+                    .addParameter("description", item.getDescription())
+                    .addParameter("imageUrl", item.getImageUrl())
+                    .addParameter("createdAt", item.getCreatedAt())
+                    .addParameter("updatedAt", item.getUpdatedAt())
+                    .addParameter("stock", item.getStock())
+                    .executeUpdate()
+                    .getKey(Integer.class);
+            return id;
+        }
     }
 
     // Read (Get all menu items)
-    public List<Item> selectAllMenuItems() throws SQLException {
+    public List<Item> selectAllMenuItems() {
         String query = "SELECT * FROM menu";
-        Statement stmt = this.conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        List<Item> items = new ArrayList<>();
-        while (rs.next()) {
-            items.add(new Item(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getDouble("price"),
-                    rs.getString("description"),
-                    rs.getString("image_url"),
-                    rs.getInt("stock"),
-                    rs.getString("created_at"),
-                    rs.getString("updated_at")
-            ));
+        try (Connection conn = sql2o.open()) {
+            return conn.createQuery(query).executeAndFetch(Item.class);
         }
-        return items;
     }
 
     // Read (Fetch a menu item by ID)
-    public Item selectMenuItemById(int id) throws SQLException {
-        String query = "SELECT * FROM menu WHERE id = ?";
-        PreparedStatement stmt = this.conn.prepareStatement(query);
-        stmt.setInt(1, id);
-        ResultSet rs = stmt.executeQuery();
-        if (rs.next()) {
-            return new Item(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getDouble("price"),
-                    rs.getString("description"),
-                    rs.getString("image_url"),
-                    rs.getInt("stock"),
-                    rs.getString("created_at"),
-                    rs.getString("updated_at")
-            );
-        } else {
-            return null;
+    public Item selectMenuItemById(int id) {
+        String query = "SELECT * FROM menu WHERE id = :id";
+        try (Connection conn = sql2o.open()) {
+            return conn.createQuery(query)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Item.class);
         }
     }
 
     // Update (Modify a menu item)
-    public void updateMenuItem(Item item) throws SQLException {
-        String query = "UPDATE menu SET name = ?, price = ?, description = ?, image_url = ?, updated_at = ?, stock = ? WHERE id = ?";
-        PreparedStatement stmt = this.conn.prepareStatement(query);
-        stmt.setString(1, item.getName());
-        stmt.setDouble(2, item.getPrice());
-        stmt.setString(3, item.getDescription());
-        stmt.setString(4, item.getImageUrl());
-        stmt.setString(5, Utility.getCurrentTimestamp());
-        stmt.setInt(6, item.getStock());
-        stmt.setInt(7, item.getId());
-        stmt.executeUpdate();
+    public void updateMenuItem(Item item) {
+        String query = "UPDATE menu SET name = :name, price = :price, description = :description, image_url = :imageUrl, " +
+                "updated_at = :updatedAt, stock = :stock WHERE id = :id";
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(query)
+                    .addParameter("name", item.getName())
+                    .addParameter("price", item.getPrice())
+                    .addParameter("description", item.getDescription())
+                    .addParameter("imageUrl", item.getImageUrl())
+                    .addParameter("updatedAt", Utility.getCurrentTimestamp())
+                    .addParameter("stock", item.getStock())
+                    .addParameter("id", item.getId())
+                    .executeUpdate();
+        }
     }
 
+
     // Delete (Remove a menu item)
-    public void deleteMenuItemById(int id) throws SQLException {
-        String query = "DELETE FROM menu WHERE id = ?";
-        PreparedStatement stmt = this.conn.prepareStatement(query);
-        stmt.setInt(1, id);
-        stmt.execute();
+    public void deleteMenuItemById(int id) {
+        String query = "DELETE FROM menu WHERE id = :id";
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(query)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 
     // Get the last inserted ID
-    public int getLastId() throws SQLException {
-        String idQuery = "SELECT LAST_INSERT_ID()";
-        Statement stmt = this.conn.createStatement();
-        ResultSet rs = stmt.executeQuery(idQuery);
-        if (rs.next()) {
-            return rs.getInt(1);  // Return the last inserted ID
-        } else {
-            throw new SQLException("Failed to retrieve generated ID");
+    public int getLastId() {
+        String query = "SELECT LAST_INSERT_ID() as id";
+        try (Connection conn = sql2o.open()) {
+            return conn.createQuery(query)
+                    .executeScalar(Integer.class);
         }
     }
 }
