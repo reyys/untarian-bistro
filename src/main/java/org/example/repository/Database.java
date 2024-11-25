155fe3abac9178bf67c65ef0f2624d6b8aca327b
Velosofy <61261357+Velosofy@users.noreply.github.com>
2024-11-25 19:51:23 +0700

    Fix SQL2O Entity Column Mapping (#3)

--- src/main/java/org/example/repository/Database.java
+++ src/main/java/org/example/repository/Database.java
@@ -1,11 +1,19 @@
 package org.example.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.sql2o.Sql2o;
 
 public class Database {
     static final Sql2o db;
 
     static {
-        db = new Sql2o("jdbc:mysql://localhost:3306/CorporateXYZ", "root", "changeme");
+        db = new Sql2o("jdbc:mysql://localhost:3306/UntarianBistro", "root", "changeme");
+        Map<String, String> colMaps = new HashMap<String, String>();
+        colMaps.put("image_url", "imageUrl");
+        colMaps.put("created_at", "createdAt");
+        colMaps.put("updated_at", "updatedAt");
+        db.setDefaultColumnMappings(colMaps);
     }
 }
