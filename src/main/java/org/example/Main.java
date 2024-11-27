9ae487eda2d295eb7436b1b31a591629af918886
Velosofy <61261357+Velosofy@users.noreply.github.com>
2024-11-27 15:01:01 +0700

    Add Admin Panel for CRUD Menu (#5)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import org.example.component.panel.AdminPanel;
+
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println("Untarian Bistro Project!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Untarian Bistro Project!");
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                new AdminPanel();
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        });
     }
 }
