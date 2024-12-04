40d52f35680f830cdbbb12167e799dec837ebc17
velosofy <rebug.fuandryputra@gmail.com>
2024-12-04 13:24:09 +0700

    Fix TabbedPane coloring

--- src/main/java/org/example/component/frame/AdminFrame.java
+++ src/main/java/org/example/component/frame/AdminFrame.java
@@ -5,6 +5,9 @@ import org.example.component.panel.OrderAdminPanel;
 import org.example.component.theme.ColorTheme;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+
+import java.awt.*;
 
 public class AdminFrame extends JFrame {
     public AdminFrame() {
@@ -15,6 +18,22 @@ public class AdminFrame extends JFrame {
 
         // Create a JTabbedPane
         JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setBackground(ColorTheme.BACKGROUND_COLOR);
+        tabbedPane.setForeground(ColorTheme.FOREGROUND_COLOR);
+        tabbedPane.setUI(new BasicTabbedPaneUI() {
+            @Override
+            protected void paintContentBorder(Graphics g, int tabPlacement, int selectedIndex) {
+                g.setColor(ColorTheme.BACKGROUND_COLOR);
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+
+            @Override
+            protected void paintTabBorder(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h,
+                    boolean isSelected) {
+                g.setColor(ColorTheme.BORDER_COLOR);
+                g.drawRect(x, y, w, h);
+            }
+        });
 
         // Add the MenuAdminPanel
         MenuAdminPanel menuAdminPanel = new MenuAdminPanel();
